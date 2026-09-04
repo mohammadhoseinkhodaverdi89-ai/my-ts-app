7ba50cb977ec12006c7b88362f60ba72f0039ede
Mohammad123456000 <mohammadhoseinkhodaverdi89@gmail.com>
2026-09-04 04:29:16 -0700

    go laptob

--- src/App.tsx
+++ src/App.tsx
@@ -7,53 +7,27 @@ import Detail from './Swiper/Detail/Detail';
 
 function App() {
   return (
-       <div className="relative min-h-screen bg-[#020617] text-white overflow-hidden">
+ <div className="relative min-h-screen bg-black text-white overflow-hidden">
+  
 
-      {/* Fixed Background */}
-      <div className="fixed inset-0 -z-10">
-        {/* Main gradient */}
-        <div className="absolute inset-0 bg-[radial-gradient(circle_at_20%_20%,rgba(0,102,179,0.18),transparent_35%),radial-gradient(circle_at_80%_70%,rgba(30,64,175,0.15),transparent_35%),linear-gradient(to_bottom,#020617,#030712,#000000)]" />
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <Header />
+            <Featured />
+            <Swiper />
+          </>
+        }
+      />
+      <Route path="/login" element={<Login />} />
+      <Route path="/detail/:id" element={<Detail />} />
+    </Routes>
+  </BrowserRouter>
 
-        {/* Blue glow */}
-        <div className="absolute -top-40 -left-40 w-[500px] h-[500px] rounded-full bg-blue-600/10 blur-[120px]" />
-
-        <div className="absolute top-[40%] -right-40 w-[500px] h-[500px] rounded-full bg-cyan-500/10 blur-[120px]" />
-
-        {/* Grid */}
-        <div
-          className="absolute inset-0 opacity-[0.035]"
-          style={{
-            backgroundImage: `
-              linear-gradient(rgba(255,255,255,0.5) 1px, transparent 1px),
-              linear-gradient(90deg, rgba(255,255,255,0.5) 1px, transparent 1px)
-            `,
-            backgroundSize: "50px 50px",
-          }}
-        />
-      </div>
-
-      <BrowserRouter>
-        <Routes>
-
-          <Route
-            path="/"
-            element={
-              <>
-                <Header />
-                <Featured />
-                <Swiper />
-              </>
-            }
-          />
-
-          <Route path="/login" element={<Login />} />
-
-          <Route path="/detail/:id" element={<Detail />} />
-
-        </Routes>
-      </BrowserRouter>
-
-    </div>
+</div>
   );
 }
 
--- src/Swiper/Detail/Detail.tsx
+++ src/Swiper/Detail/Detail.tsx
@@ -1,12 +1,17 @@
 import { useParams } from "react-router-dom";
 import Photo1 from './Photo/Gemini_Generated_Image_e2q6yhe2q6yhe2q6.jpg'
+import { IoFlashOutline } from "react-icons/io5";
+import { IoMdSpeedometer } from "react-icons/io";
+import { PiEngineLight } from "react-icons/pi";
+import { TbManualGearbox } from "react-icons/tb";
+
 interface Todo  {
     img : string;
     col : string;
     name: string;
     model : string;
     detail : string;
-    info : string;
+    info?:string;
     id : number;
 }
 
@@ -15,7 +20,7 @@ type RouteParams = {
 }
 
 const BmwDetail : Todo[] = [
-    {img : Photo1 , col : "2026 collection" , name :  "BMW M5" , model : "competion" , detail : "dwadawdadadawaw" , info : "awawa" , id : 1 }
+    {img : Photo1 , col : "2026 collection" , name :  "BMW M5" , model : "competion" , detail : "the ultimate blend op performance and luxury " , info : "awawa" , id : 1 }
 ]
 function Detail() {
   const { id } = useParams<RouteParams>();
@@ -27,16 +32,52 @@ function Detail() {
   }
 
   return (
-    <div className="text-white min-h-screen w-full relative">
-      <div className="w-full h-full">
-      <img src={detail.img} alt="photo" className="w-full h-full object-cover object-center"/>
+    <div className="text-white min-h-screen w-full relative overflow-hidden">
+      <div className="w-full h-full absolute inset-0">
+      <img src={detail.img} alt="photo" className="w-full h-1/2 object-center object-cover"/>
       </div>
-      <div className="absolute translate-y-1/2 top-0 right-0 p-3">
-          <p className="bg-blue-500/20 rounded-sm flex justify-center text-blue-300 text-sm">{detail.col}</p>
-      <h1>{detail.name}</h1>
-      <h1>{detail.model}</h1>
-      <p>{detail.detail}</p>
-      <p>{detail.info}</p>
+      <div className="hidden md:flex md:flex-col h-1/2 top-10 right-4 absolute">
+      <div className="flex flex-row items-center">
+      <span className="w-[2px] h-4 bg-blue-300 "></span>
+        <p className="bg-blue-500/20 text-blue-300 text-[10px] px-2 py-0.5 rounded-sm font-medium text-center">{detail.col}</p>
+      </div>
+      <h1 className="font-inter text-5xl">{detail.name}</h1>
+      <h1 className="font-montserrat text-4xl">{detail.model}</h1>
+      <p className="font-medium text-[15px] max-w-70">{detail.detail}</p>
+
+      <div className="flex flex-row">
+      <div className="flex flex-row justify-center items-center">
+        <IoFlashOutline />
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-[10px]">625</p>
+          <p>HP</p>
+        </div>
+      </div>
+
+      <div className="flex flex-row justify-center items-center">
+      <IoMdSpeedometer />
+        <div className="flex flex-col justify-center items-center">
+          <p>3.3s</p>
+          <p>0-100 km/h </p>
+        </div>
+      </div>
+
+      <div className="flex flex-row justify-center items-center">
+      <PiEngineLight />
+        <div className="flex flex-col justify-center items-center">
+          <p>4.4L V8</p>
+          <p>Twin Turbo </p>
+        </div>
+      </div>
+
+       <div className="flex flex-row justify-center items-center">
+      <TbManualGearbox />
+        <div className="flex flex-col justify-center items-center">
+          <p>8 speed</p>
+          <p>M Steptronic</p>
+        </div>
+      </div>
+      </div>
       </div>
     </div>
   );
